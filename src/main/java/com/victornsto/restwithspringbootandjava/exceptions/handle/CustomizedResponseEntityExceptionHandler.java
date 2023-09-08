0611549dad2bc247f310c1d7262948fdb1720db2
Victor Nascimento <victor.nascimento@techlead.com.br>
2023-09-08 14:17:20 -0300

    Math controller

---
+++ src/main/java/com/victornsto/restwithspringbootandjava/exceptions/handle/CustomizedResponseEntityExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.victornsto.restwithspringbootandjava.exceptions.handle;
+
+import com.victornsto.restwithspringbootandjava.exceptions.ExceptionsResponse;
+import com.victornsto.restwithspringbootandjava.exceptions.UnsupportedMathOperationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.Date;
+
+@RestController
+@ControllerAdvice
+// Caso ocorra uma exceção na classe controler e ele não seja tratado na própria classe, ele cai na exceção do controller advice
+public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(Exception.class)
+    public final ResponseEntity<ExceptionsResponse> handleAllExceptions(Exception ex, WebRequest request){
+        ExceptionsResponse exceptionsResponse = new ExceptionsResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(exceptionsResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(UnsupportedMathOperationException.class)
+    public final ResponseEntity<ExceptionsResponse> handleBadRequestExceptions(Exception ex, WebRequest request){
+        ExceptionsResponse exceptionsResponse = new ExceptionsResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false)
+        );
+        return new ResponseEntity<>(exceptionsResponse, HttpStatus.BAD_REQUEST);
+    }
+}
